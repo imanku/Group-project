9b5e80ab5c3f652721ee9865f2b8a09361271e36
imanku <43012896+imanku@users.noreply.github.com>
2018-12-11 15:28:57 -0500

    inderjot commit

--- main.cpp
+++ main.cpp
@@ -634,3 +634,552 @@ void shirtlist()
 }
 
 
+void paintlist()
+{
+int i,j,z;
+    int Amount=100;
+    int Samount=200;
+    int Tamount=300;
+    double Discount,totalAmount;
+    do{
+    cout<<" press 1 to buy lower $"<<Amount<<endl;
+    cout<<" press 2 to buy simple short $"<<Samount<<endl;
+    cout<<" press 3 to buy jean shorts  $"<<Tamount<<endl;
+    cin>>i;
+    if (i==1)
+    {
+     cout<<"******* Thanks for taking lower ******* "<<endl;
+     cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     if(j==2){
+         
+       Discount=10/100*Amount;
+       totalAmount=Amount-Discount;
+       cout<<" "<<endl;
+       cout<<" Your Ammount  is $"<<totalAmount<<endl;  
+       cout<<" "<<endl;
+       cout<<"**************** Thanks for visting ********************* "<<endl;
+        cout<<"**************** Have  a  good day! ********************* "<<endl;           
+         }
+         while(j==3);}
+
+
+
+
+         else if(i==2){
+            cout<<" *************** Thanks for taking simple shorts ************* "<<endl;
+             cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if(i==2)
+     {
+          Discount=10/100*Samount;
+    totalAmount=Samount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     while(j==3);}
+
+
+
+
+
+     else if(i==3)
+     {
+         cout<<"******************* Thanks for taking jean shorts ******************** "<<endl;
+          cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if (j==2){
+          Discount=10/100*Tamount;
+    totalAmount=Tamount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }}}
+     while(j==3);
+           }
+     
+     void womencloth()
+     {
+        
+          int d,h;
+    char e[10],f[10],g[10];
+cout<<" ============================================== "<<endl;
+cout<<"             Thanks for choosing women cloths   "<<endl;
+cout<<" ============================================== "<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<" * Press 1 for sports cloths *"<<endl;
+cout<<" * press 2 for casual cloths *"<<endl;
+cout<<" * Press 3 for formal cloths *"<<endl;
+cin>>d;
+if(d==1){
+    cout<<" *****Thanks For choosing sports Cloths ****** "<<endl;
+    cout<<" "<<endl;
+    cout<<" "<<endl;
+    cout<<" * Enter your Brand name : ";
+    cin>>e;
+    cout<<" * Enter your Size : ";
+    cin>>f;
+    cout<<" * Enter your color : ";
+    cin>>g;
+    cout<<"         "<<endl;
+    cout<<"         "<<endl;
+    cout<<" * perss 1 for T-Shirts , upper, Jackets  :  "<<endl;
+    cout<<" * press 2 for lower   , simple shorts, jean shorts  :  "<<endl;
+    cin>>h;
+    if(h==1){
+        shirtlist();
+
+    }
+    else if(h==2){
+      paintlist();
+    } }
+    
+ else if (d==2)
+    {
+        cout<<" *****Thanks For choosing casual Cloths ****** "<<endl;
+    cout<<" "<<endl;
+    cout<<" "<<endl;
+    cout<<" * Enter your Brand name : ";
+    cin>>e;
+    cout<<" * Enter your Size : ";
+    cin>>f;
+    cout<<" * Enter your color : ";
+    cin>>g;
+    cout<<"         "<<endl;
+    cout<<"         "<<endl;
+    cout<<" * perss 1 for  Shirts , Hoodies ,  snow Jackets  :  "<<endl;
+    cout<<" * press 2 for  sweaters , sleepwears , jeans    :  "<<endl;
+    cin>>h;
+    if(h==1)
+    {
+       mencasual();
+    }
+    else if (h==2)
+    {
+     mencasualtwo();
+    }}
+
+    else if(d==3)
+    {
+
+         cout<<" *****Thanks For choosing casual Cloths ****** "<<endl;
+    cout<<" "<<endl;
+    cout<<" "<<endl;
+    cout<<" * Enter your Brand name : ";
+    cin>>e;
+    cout<<" * Enter your Size : ";
+    cin>>f;
+    cout<<" * Enter your color : ";
+    cin>>g;
+    cout<<"         "<<endl;
+    cout<<"         "<<endl;
+    cout<<" * perss 1 for  Shirts , Hoodies ,  snow Jackets  :  "<<endl;
+    cout<<" * press 2 for  sweaters , sleepwears , jeans    :  "<<endl;
+    cin>>h;
+    if(h==1){
+        menformal();
+
+    }
+
+    }
+
+
+
+    }
+
+     
+    
+void mencasual()
+{
+int i,j;
+     int Amount=100;
+    int Samount=200;
+    int Tamount=300;
+    double Discount,totalAmount;
+    do{
+    cout<<" press 1 to buy shirt $"<<Amount<<endl;
+    cout<<" press 2 to buy Hoodies $"<<Samount<<endl;
+    cout<<" press 3 to buy snow Jackets $"<<Tamount<<endl;
+    cin>>i;
+    if (i==1)
+    {
+     cout<<" ******* Thanks for taking Shirt ******* "<<endl;
+     cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     if(j==2){
+       Discount=10/100*Amount;
+    totalAmount=Amount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is "<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;           
+
+     }
+    
+    while(j==3);
+
+    }
+
+    
+         else if(i==2){
+            cout<<" *************** Thanks for taking Hoodies  ************* "<<endl;
+             cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if(j==2)
+     {
+          Discount=10/100*Samount;
+    totalAmount=Samount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     while(j==3);}
+
+
+
+
+
+     else if(i==3)
+     {
+         cout<<"******************* Thanks for taking snow jacket ******************** "<<endl;
+          cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if (j==2){
+          Discount=10/100*Tamount;
+    totalAmount=Tamount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }}}
+     while(j==3);
+
+}
+
+
+
+void mencasualtwo()
+{
+
+    int i,j;
+     int Amount=100;
+    int Samount=200;
+    int Tamount=300;
+    double Discount,totalAmount;
+    do{
+    cout<<" press 1 to buy sweater $"<<Amount<<endl;
+    cout<<" press 2 to buy sleepwears $"<<Samount<<endl;
+    cout<<" press 3 to buy Jeans  $"<<Tamount<<endl;
+    cin>>i;
+    if (i==1)
+    {
+     cout<<" ******* Thanks for taking sweater ******* "<<endl;
+     cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     if(j==2){
+       Discount=10/100*Amount;
+    totalAmount=Amount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is "<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;           
+
+     }
+    
+    while(j==3);
+
+    }
+
+    
+         else if(i==2){
+            cout<<" *************** Thanks for taking sleepwear  ************* "<<endl;
+             cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if(j==2)
+     {
+          Discount=10/100*Samount;
+    totalAmount=Samount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     while(j==3);}
+
+
+
+
+
+     else if(i==3)
+     {
+         cout<<"******************* Thanks for taking jeans ******************** "<<endl;
+          cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if (j==2){
+          Discount=10/100*Tamount;
+    totalAmount=Tamount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }}}
+     while(j==3); 
+
+}
+void menformal()
+{
+
+ int i,j;
+     int Amount=100;
+    int Samount=200;
+    int Tamount=300;
+    double Discount,totalAmount;
+    do{
+    cout<<" press 1 to buy half sleves shirt $"<<Amount<<endl;
+    cout<<" press 2 to buy full sleves shirt $"<<Samount<<endl;
+    cout<<" press 3 to buy cotton shirt  $"<<Tamount<<endl;
+    cin>>i;
+    if (i==1)
+    {
+     cout<<" ******* Thanks for taking half sleves shirt ******* "<<endl;
+     cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     if(j==2){
+       Discount=10/100*Amount;
+    totalAmount=Amount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is "<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;           
+
+     }
+    
+    while(j==3);
+
+    }
+
+    
+         else if(i==2){
+            cout<<" *************** Thanks for taking full sleves shirt  ************* "<<endl;
+             cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if(j==2)
+     {
+          Discount=10/100*Samount;
+    totalAmount=Samount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     while(j==3);}
+
+
+
+
+
+     else if(i==3)
+     {
+         cout<<"******************* Thanks for taking cotton shirt  ******************** "<<endl;
+          cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if (j==2){
+          Discount=10/100*Tamount;
+    totalAmount=Tamount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }}}
+     while(j==3); 
+
+}
+void menformaltwo()
+{
+
+     int i,j;
+     int Amount=100;
+    int Samount=200;
+    int Tamount=300;
+    double Discount,totalAmount;
+    do{
+    cout<<" press 1 to buy half  simple  paint $"<<Amount<<endl;
+    cout<<" press 2 to buy full designer paint $"<<Samount<<endl;
+    cout<<" press 3 to buy       suit  $"<<Tamount<<endl;
+    cin>>i;
+    if (i==1)
+    {
+     cout<<" ******* Thanks for taking  simple paint ******* "<<endl;
+     cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     if(j==2){
+       Discount=10/100*Amount;
+    totalAmount=Amount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is "<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;           
+
+     }
+    
+    while(j==3);
+
+    }
+
+    
+         else if(i==2){
+            cout<<" *************** Thanks for taking designer Piant  ************* "<<endl;
+             cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if(j==2)
+     {
+          Discount=10/100*Samount;
+    totalAmount=Samount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your payment is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     while(j==3);}
+    
+
+
+ else if(i==3)
+     {
+         cout<<"******************* Thanks for taking suit  ******************** "<<endl;
+          cout<<"           "<<endl;
+     cout<<" press 1 to make payment "<<endl;
+     cout<<" press 2 to take discount "<<endl;
+     cout<<" press 3 for back to list "<<endl;
+     cin>>j;
+     if (j==1){
+         cout<<"**************** Thanks for visting ********************* "<<endl;
+         cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }
+     else if (j==2){
+          Discount=10/100*Tamount;
+    totalAmount=Tamount-Discount;
+    cout<<" "<<endl;
+    cout<<" Your Ammount is $"<<totalAmount<<endl;  
+    cout<<" "<<endl;
+    cout<<"**************** Thanks for visting ********************* "<<endl;
+    cout<<"**************** Have  a  good day! ********************* "<<endl;  
+     }}}
+     while(j==3); 
+}
